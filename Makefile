5bf733092ddd7c3f5410eb8b02b080146fa81309
Your Name <delamerde@gmail.com>
2024-02-13 09:30:48 +0100

    Makefile and added user and fixed my shit

--- Makefile
+++ Makefile
@@ -2,14 +2,27 @@ COMPOSE_F 	= srcs/docker-compose.yml
 ENV_F		= srcs/.env
 
 VOLUMES_DIR		:=	db wordpress
-VOLUMES_PATH	:=	/home/data/inception_data
+VOLUMES_PATH	:=	/home/hgeffroy/data/
 VOLUMES			:=	$(addprefix $(VOLUMES_PATH)/, $(VOLUMES_DIR))
 
 all:
 	mkdir -p $(VOLUMES)
 	docker compose -f $(COMPOSE_F) --env-file=$(ENV_F) up -d --build
 
+up:
+	docker compose -f $(COMPOSE_F) --env-file=$(ENV_F) up -d
+
+down:
+	docker compose down
+
+re: down up
+
 clean:
-	docker stop $$(docker ps -qa)
-	docker system prune -a --force
-	docker volume rm $$(docker volume ls)
+	docker rmi -f $$(docker images -qa);
+	docker stop $$(docker ps -qa);
+	docker system prune -a --force;
+	docker volume rm $$(docker volume ls -q);
+	docker network rm $$(docker notwork ls -q);
+	rm -rf $(VOLUMES)
+
+.PHONY: re clean up down all
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-echo "lala"
+sleep 10
 
 if [ ! -f "/var/www/html/wp-config.php" ]; then
     wp core download --path=/var/www/html --allow-root
     wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=mariadb:3306 --path=/var/www/html --allow-root
-    wp core install --url="$DOMAIN_NAME" --title="Inception" --admin_user="$WP_USER" --admin_password="$WP_PASS" --admin_email="$WP_MAIL" --skip-email --path=/var/www/html --allow-root
+    wp core install --url="$DOMAIN_NAME" --title="Inception" --admin_user="$WP_ADMIN" --admin_password="$WP_ADMINPASS" --admin_email="$WP_ADMINMAIL" --skip-email --path=/var/www/html --allow-root
+    wp user create --user-login="$WP_USER" --user-pass="$WP_USERPASS" --user-email="WP_USERMAIL" --path=/var/www/html --allow-root
 fi
 
-mkdir -p /run/php/ # ?
+mkdir -p /run/php/
 php-fpm7.4 -F
